e10c55419b102e3b00c06cac724450b42ba8ea1f
malfunctionize <malfunctionize@protonmail.com>
2023-05-05 17:40:56 -0400

    i

---
+++ models.js
@@ -0,0 +1,67 @@
+[{
+  "name": "pygmalion-7b-f16-ggml",
+  "items": [{
+    "url": "https://huggingface.co/sasha0552/pygmalion-7b-f16-ggml/resolve/main/ggml-model-f16.bin",
+    "name": "ggml-model-f16.bin"
+  }]
+}, {
+  "name": "stable-vicuna-13B-GGML",
+  "items": [{
+    "url": "https://huggingface.co/TheBloke/stable-vicuna-13B-GGML/resolve/main/stable-vicuna-13B.ggml.q4_0.bin",
+    "name": "stable-vicuna-13B.ggml.q4_0.bin"
+  }, {
+    "url": "https://huggingface.co/TheBloke/stable-vicuna-13B-GGML/resolve/main/stable-vicuna-13B.ggml.q4_2.bin",
+    "name": "stable-vicuna-13B.ggml.q4_2.bin"
+  }, {
+    "url": "https://huggingface.co/TheBloke/stable-vicuna-13B-GGML/resolve/main/stable-vicuna-13B.ggml.q5_0.bin",
+    "name": "stable-vicuna-13B.ggml.q5_0.bin"
+  }, {
+    "url": "https://huggingface.co/TheBloke/stable-vicuna-13B-GGML/resolve/main/stable-vicuna-13B.ggml.q5_1.bin",
+    "name": "stable-vicuna-13B.ggml.q5_1.bin"
+  }]
+}, {
+  "name": "gpt4all",
+  "items": [{
+    "url": "https://huggingface.co/frankenstyle/ggml-q4-models/resolve/main/models/gpt4all/7B/gpt4all-lora-quantized.bin",
+    "name": "gpt4all-lora-quantized.bin"
+  }]
+}, {
+  "name": "Koala-7B-ggml-q5_1",
+  "items": [{
+    "url": "https://huggingface.co/xzuyn/Koala-7B-ggml-q5_1/resolve/main/ggml-model-q5_1.bin"
+    "name": "ggml-model-q5_1.bin"
+  }]
+}, {
+  "name": "koala-7b-ggml-q4_0",
+  "items": [{
+    "url": "https://huggingface.co/gorborukov/koala-7b-ggml-q4_0/resolve/main/koala-7b-ggml-q4_0.bin",
+    "name": "koala-7b-ggml-q4_0.bin"
+  }]
+}, {
+  "name": "koala-7b-ggml-q4_1",
+  "items": [{
+    "url": "https://huggingface.co/mongolian-basket-weaving/koala-7b-ggml-q4_1/resolve/main/ggml-model-q4_1.bin",
+    "name": "ggml-model-q4_1.bin"
+  }]
+}, {
+  "name": "OpenLLaMa-7B",
+  "items": [{
+    "url": "https://huggingface.co/xzuyn/OpenLLaMa-Preview-200BT-7B-ggml-q5_0/resolve/main/ggml-model-q5_0.bin",
+    "name": "ggml-model-q5_0.bin"
+  }, {
+    "url": "https://huggingface.co/xzuyn/OpenLLaMa-Preview-200BT-7B-ggml-q5_1/resolve/main/ggml-model-q5_1.bin",
+    "name": "ggml-model-q5_1.bin"
+  }]
+}, {
+  "name": "wizardlm-7B-unfiltered-GGML",
+  "items":[{
+    "url": "https://huggingface.co/Bakanayatsu/llama7b-wizardlm-unfiltered-GGML/resolve/main/wizardlm-7B-unfiltered-GGML-q4_0.bin",
+    "name": "wizardlm-7B-unfiltered-GGML-q4_0.bin"
+  }, {
+    "url": "https://huggingface.co/Bakanayatsu/llama7b-wizardlm-unfiltered-GGML/resolve/main/wizardlm-7B-unfiltered-GGML-q4_2.bin",
+    "name": "wizardlm-7B-unfiltered-GGML-q4_2.bin"
+  }, {
+    "url": "https://huggingface.co/Bakanayatsu/llama7b-wizardlm-unfiltered-GGML/resolve/main/wizardlm-7B-unfiltered-GGML-q5_1.bin",
+    "name": "wizardlm-7B-unfiltered-GGML-q5_1.bin"
+  }]
+}]
